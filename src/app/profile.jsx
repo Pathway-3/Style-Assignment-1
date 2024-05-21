6bde67d9ded86dde7225414510c3e03bfaa2cbb6
jordan toujouse <jtoujo1@gmail.com>
2024-05-20 22:27:06 -0500

    Removed useState and useEffect, imported UserProfile component, and passed props to it

--- src/app/profile.jsx
+++ src/app/profile.jsx
@@ -1,16 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 export default function profile({ userName, userBio, userHobbies }) {
-  const [userName, setUserName] = useState("");
-  const [userBio, setUserBio] = useState("");
-  const [userHobbies, setUserHobbies] = useState([]);
-
-  useEffect(() => {
-    console.log("Component mounted");
-  }, []);
-
   return (
     <div>
       <h1>{!userName ? "Missing User Info" : userName}</h1>
@@ -25,6 +18,11 @@ export default function profile({ userName, userBio, userHobbies }) {
         <p>No Hobbies Listed</p>
       )}
       <button onClick={() => alert("Hello World")}>Click Me</button>
+      <UserProfile
+        userName={userName}
+        userBio={userBio}
+        userHobbies={userHobbies}
+      />
     </div>
   );
 }
